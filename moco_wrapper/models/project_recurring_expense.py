b2dd887286de89be636d34552526f5e04f1d0c90
sommalia <sommalia@protonmail.com>
2019-09-10 16:53:16 +0200

    start working on recurring project expenses

--- moco_wrapper/models/project_recurring_expense.py
+++ moco_wrapper/models/project_recurring_expense.py
@@ -6,3 +6,73 @@ class ProjectRecurringExpense(MocoBase):
 
     def __init__(self, moco):
         self._moco = moco
+
+    def getlist(
+        self,
+        project_id,
+        sort_by = None,
+        sort_order = 'asc'
+        ):
+        """retrieve a list of recurring expenses on a project
+
+        :param project_id: id of the project the expesen belongs to
+        :param sort_by: field to sort results by
+        :param sort_order: asc or desc
+        :returns: list of recurring expenses
+        """
+        pass
+
+    def get(
+        self,
+        project_id,
+        recurring_expense_id,
+        ):
+        """retrieve a single recurring expense
+
+        :param project_id: id of the project the expesen belongs to
+        :param recurring_expense_id: id of the recurring expense
+        :returns: a single recurring expense object
+        """
+        pass
+
+    def create(
+        self,
+        project_id,
+        start_date,
+        period,
+        title, 
+        quantity,
+        unit,
+        unit_price,
+        unit_cost,
+        finish_date = None,
+        description = None,
+        billable = None,
+        budget_relevant = None,
+        custom_properties = None,
+        ):
+        pass
+
+    def update(
+        self,
+        project_id,
+        recurring_expense_id,
+        title = None, 
+        quantity = None,
+        unit = None,
+        unit_price = None,
+        unit_cost = None,
+        finish_date = None,
+        description = None,
+        billable = None,
+        budget_relevant = None,
+        custom_properties = None,
+        ):
+        pass
+
+    def delete(
+        self,
+        project_id,
+        recurring_expense_id,
+        ):
+        pass
