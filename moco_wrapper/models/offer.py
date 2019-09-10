9725ce61d3ce4989059e139108e923bdae53caaa
sommalia <sommalia@protonmail.com>
2019-09-10 13:25:51 +0200

    implement offer

--- moco_wrapper/models/offer.py
+++ moco_wrapper/models/offer.py
@@ -6,3 +6,62 @@ class Offer(MocoBase):
 
     def __init__(self, moco):
         self._moco = moco
+
+    def getlist(
+        self,
+        status = None,
+        from_date = None,
+        to_date = None,
+        identifier = None,
+        sort_by = None,
+        sort_order = 'asc'
+        ):
+        """retrieve a list of offers
+
+        :param status: offer status ("created", "sent", "accepted", "billed", "archived")
+        :param from_date: starting filter date (format YYYY-MM-DD)
+        :param to_date: ending filter date (format YYYY-MM-DD)
+        :param identifier: offer identifier string (ex: "A1903-003")
+        :param sort_by: field to sort the results by
+        :param sort_order: asc or desc
+        :returns: list of offer objects
+        """
+        params = {}
+        for key, value in (
+            ("status", status),
+            ("from", from_date),
+            ("to", to_date),
+            ("identifier", identifier)
+        ):
+            if value is not None:
+                params[key] = value
+
+        if sort_by is not None:
+            params["sort_by"] = "{} {}".format(sort_by, sort_order)
+
+        return self._moco.get(API_PATH["offer_getlist"], params=params)
+
+    def get(
+        self,
+        id
+        ):
+        """retrieve a sigle offer
+
+        :param id: id of the offer
+        :returns: single offer object
+
+        """
+        return self._moco.get(API_PATH["offer_get"].format(id=id))
+
+    def get_doc(
+        self,
+        id,
+        letter_paper_id = None
+        ):
+        """retrive the offer document for a single offer
+
+        :param id: id of the offer
+        :param letter_paper_id: id of the letter paper (default white)
+        :returns: filestream of the document
+        """
+        return self._moco.get(API_PATH["offer_get_doc"].format(id=id))
--- tests/unit/models/test_offer.py
+++ tests/unit/models/test_offer.py
@@ -3,16 +3,46 @@ import pytest
 
 class TestOffer(UnitTest):
     def test_getlist(self):
-        assert False
+        status = "created"
+        from_date = '2019-10-10'
+        to_date = '2020-10-10'
+        identifier = 'OFFER-10-21'
+
+        response = self.moco.Offer.getlist(status=status, from_date=from_date, to_date=to_date, identifier=identifier)
+        params = response["params"]
+
+        assert params["status"] == status
+        assert params["from"] == from_date
+        assert params["to"] == to_date
+        assert params["identifier"] == identifier
+
+        assert response["method"] == "GET"
 
     def test_getlist_sort_default(self):
-        assert False
+        sort_by = "field to sort by"
+
+        response = self.moco.Offer.getlist(sort_by=sort_by)
+
+        assert response["params"]["sort_by"] == "{} asc".format(sort_by)
 
     def test_getlist_sort_overwrite(self):
-        assert False
+        sort_by = "field to sort by"
+        sort_order = "desc"
+
+        response = self.moco.Offer.getlist(sort_by=sort_by, sort_order=sort_order)
+
+        assert response["params"]["sort_by"] == "{} {}".format(sort_by, sort_order)
 
     def test_get(self):
-        assert False
+        offer_id = 123
+
+        response = self.moco.Offer.get(offer_id)
+
+        assert response["method"] == "GET"
 
     def test_get_doc(self):
-        assert False
+        offer_id = 123
+
+        response = self.moco.Offer.get_doc(offer_id)
+
+        assert response["method"] == "GET"
