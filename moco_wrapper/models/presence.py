269a7b953d4c336e4fe6b722d9a2a428dcae3e23
sommalia <sommalia@protonmail.com>
2019-09-10 13:01:53 +0200

    implement expense

--- moco_wrapper/models/presence.py
+++ moco_wrapper/models/presence.py
@@ -6,3 +6,119 @@ class Presence(MocoBase):
 
     def __init__(self, moco):
         self._moco = moco
+
+    def getlist(
+        self,
+        from_date = None,
+        to_date = None,
+        user_id = None,
+        sort_by = None,
+        sort_order = 'asc'
+        ):
+        """retrieve all presences
+
+        :param from_date: starting date (must be provided together with to_date) (format YYYY-MM-DD)
+        :param to_date: end date (must be provided together with from_date) (format YYYY-MM-DD)
+        :param user_id: id of the user
+        :param sort_by: field to sort results by
+        :param sort_order: asc or desc
+        :returns: list of presence objets
+        """
+        params = {}
+
+        for key, value in (
+            ("from", from_date),
+            ("to", to_date),
+            ("user_id", user_id)
+        ):
+            if value is not None:
+                params[key] = value
+
+        if sort_by is not None:
+            params["sort_by"] = "{} {}".format(sort_by, sort_order)
+
+        return self._moco.get(API_PATH["presence_getlist"], params=params)
+
+    def get(
+        self,
+        id
+        ):
+        """retrieve a single presence
+
+        :param id: id of the presence
+        :returns: a single presence object
+        """
+        return self._moco.get(API_PATH["presence_get"].format(id=id))
+
+    def create(
+        self,
+        date,
+        from_time,
+        to_time = None
+        ):
+        """create a presence
+
+        :param date: date of the presence (format YYYY-MM-DD)
+        :param from_time: starting time of the presence (format HH:MM)
+        :param to_time: end time of the presence (format HH:MM)
+        :returns: the created presence 
+        """
+        data = {
+            "date": date,
+            "from": from_time
+        }
+
+        for key, value in (
+            ("to", to_time),
+        ):
+            if value is not None:
+                data[key] = value
+
+        return self._moco.post(API_PATH["presence_create"], data=data)
+
+    def touch(
+        self,
+        ):
+        """creates a new presence for the user with the corresponding api key starting from the current time. Or it terminates an existing open presence at the current time. Can be used to implement a clock system (RFID)
+
+        if a presence is started and stopped within the same minute, it will be discarded
+        """ 
+        return self._moco.post(API_PATH["presence_touch"])
+
+    def update(
+        self,
+        id,
+        date = None,
+        from_time = None,
+        to_time = None
+        ):
+        """update a presence
+
+        :param id: id of the presence
+        :param date: date of the presence (format YYYY-MM-DD)
+        :param from_time: starting time of the presence (format HH:MM)
+        :param to_time: end time of the presence (format HH:MM)
+        :returns: the created presence 
+        """
+        data = {}
+        for key, value in (
+            ("date", date),
+            ("from", from_time),
+            ("to", to_time),
+        ):
+            if value is not None:
+                data[key] = value
+
+        return self._moco.put(API_PATH["presence_update"].format(id=id), data=data)
+        
+    def delete(
+        self,
+        id
+        ):
+        """deletes a presence
+
+        :param id: id of the presence
+        """
+        return self._moco.delete(API_PATH["presence_delete"].format(id=id))
+
+
--- tests/unit/models/test_presence.py
+++ tests/unit/models/test_presence.py
@@ -5,25 +5,78 @@ import pytest
 class TestPresence(UnitTest):
 
     def test_getlist(self):
-        assert False
+        from_date = '2019-10-10'
+        to_date = '2020-10-10'
+        user_id = 4
+
+        response = self.moco.Presence.getlist(from_date=from_date, to_date=to_date, user_id=user_id)
+        params = response["params"]
+
+        assert params["from"] == from_date
+        assert params["to"] == to_date
+        assert params["user_id"] == user_id
+        assert response["method"] == "GET"
+
 
     def test_getlist_sort_default(self):
-        assert False
+        sort_by = "test field to sort by"
+
+        response = self.moco.Presence.getlist(sort_by=sort_by)
+
+        assert response["params"]["sort_by"] == "{} asc".format(sort_by)
 
     def test_getlist_sort_overwrite(self):
-        assert False
+        sort_by = "test field to sort by"
+        sort_order = "desc"
+
+        response = self.moco.Presence.getlist(sort_by=sort_by, sort_order=sort_order)
+
+        assert response["params"]["sort_by"] == "{} {}".format(sort_by, sort_order)
 
     def test_get(self):
-        assert False
+        presence_id = 123
+
+        response = self.moco.Presence.get(presence_id)
+
+        assert response["method"] == "GET"
 
     def test_create(self):
-        assert False
+        date = '2019-10-10'
+        from_time = '08:00'
+        to_time = '14:00'
+
+        response = self.moco.Presence.create(date, from_time, to_time=to_time)
+        data = response["data"]
+
+        assert data["date"] == date
+        assert data["from"] == from_time
+        assert data["to"] == to_time
+        assert response["method"] == "POST"
 
     def test_touch(self):
-        assert False
+        
+        response = self.moco.Presence.touch()
+
+        assert response["method"] == "POST"
     
     def test_update(self):
-        assert False
+        presence_id = 123
+        date = '2019-10-10'
+        from_time = '08:00'
+        to_time = '14:00'
+
+        response = self.moco.Presence.update(presence_id, date=date, to_time=to_time, from_time=from_time)
+        data = response["data"]
+
+        assert data["date"] == date
+        assert data["from"] == from_time
+        assert data["to"] == to_time
+        assert response["method"] == "PUT"
+
 
     def test_delete(self):
-        assert False
+        presence_id = 123
+
+        response = self.moco.Presence.delete(presence_id)
+
+        assert response["method"] == "DELETE"
