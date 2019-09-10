ff431862eb0f0899940a92d041f546a14ac76196
sommalia <sommalia@protonmail.com>
2019-09-10 12:36:00 +0200

    implement project task

--- moco_wrapper/models/project_task.py
+++ moco_wrapper/models/project_task.py
@@ -7,3 +7,116 @@ class ProjectTask(MocoBase):
     def __init__(self, moco):
         self._moco = moco
 
+    def getlist(
+        self,
+        project_id,
+        sort_by = None,
+        sort_order = 'asc'
+        ):
+        """retrieve a list of task for a project
+
+        :param project_id: id of the project
+        :param sort_by: field to sort results by
+        :param sort_order: asc or desc
+        :returns: list of project tasks
+        """
+        params = {}
+        if sort_by is not None:
+            params["sort_by"] = "{} {}".format(sort_by, sort_order)
+
+
+        return self._moco.get(API_PATH["project_task_getlist"].format(project_id=project_id), params=params)
+
+    def get(
+        self,
+        project_id,
+        task_id
+        ):
+        """retrieve a single project task
+
+        :param project_id: id of the project the task belongs to
+        :param task:id: id of the task
+        :returns: a single project task
+        """
+        return self._moco.get(API_PATH["project_task_get"].format(project_id=project_id, task_id=task_id))
+
+    def create(
+        self,
+        project_id,
+        name,
+        billable = None,
+        active = None,
+        budget = None,
+        hourly_rate = None
+        ):
+        """create a task on a project
+
+        :param project_id: id of the project the created task will belong to
+        :param name: name of the task
+        :param billable: true/false, if this task is billable or not
+        :param active: true/false, if this task is active or not
+        :param budget: how much budget for the task (ex: 200)
+        :param hourly_rate: how much is the hourly rate for the task (ex: 120)
+        :returns: the created project task
+        """
+        data = {
+            "name": name
+        }
+
+        for key, value in (
+            ("billable", billable),
+            ("active", active),
+            ("budget", budget),
+            ("hourly_rate", hourly_rate)
+        ):
+            if value is not None:
+                data[key] = value
+
+        return self._moco.post(API_PATH["project_task_create"].format(project_id=project_id), data=data)
+
+    def update(
+        self,
+        project_id,
+        task_id,
+        name = None,
+        billable = None,
+        active = None,
+        budget = None,
+        hourly_rate = None
+        ):
+        """update a task on a project
+
+        :param project_id: id of the project the task belongs to
+        :param task_id: id of the task to update
+        :param name: name of the task
+        :param billable: true/false, if this task is billable or not
+        :param active: true/false, if this task is active or not
+        :param budget: how much budget for the task (ex: 200)
+        :param hourly_rate: how much is the hourly rate for the task (ex: 120)
+        :returns: the created project task
+        """
+        data = {}
+        for key, value in (
+            ("name", name),
+            ("billable", billable),
+            ("active", active),
+            ("budget", budget),
+            ("hourly_rate", hourly_rate)
+        ):  
+            if value is not None:
+                data[key] = value
+
+        return self._moco.put(API_PATH["project_task_update"].format(project_id=project_id, task_id=task_id), data=data)
+
+    def delete(
+        self,
+        project_id,
+        task_id
+        ):
+        """deletes a task on a projects
+
+        only possible as long as no hours were tracked on the task yet
+        """
+
+        return self._moco.delete(API_PATH["project_task_delete"].format(project_id=project_id, task_id=task_id))
+
--- tests/unit/models/test_project_task.py
+++ tests/unit/models/test_project_task.py
@@ -3,22 +3,79 @@ import pytest
 
 class TestProjectTask(UnitTest):
     def test_getlist(self):
-        assert False
+        project_id = 2
+
+        response = self.moco.ProjectTask.getlist(project_id)
+
+        assert response["method"] == "GET"
 
     def test_getlist_sort_default(self):
-        assert False
+        project_id = 2
+        sort_by = "field to sort by"
+
+        response = self.moco.ProjectTask.getlist(project_id, sort_by=sort_by)
+
+        assert response["params"]["sort_by"] == "{} asc".format(sort_by)
 
     def test_getlist_sort_overwrite(self):
-        assert False
+        project_id = 2
+        sort_by = "field to sort by"
+        sort_order = "desc"
+
+        response = self.moco.ProjectTask.getlist(project_id, sort_by=sort_by, sort_order=sort_order)
+
+        assert response["params"]["sort_by"] == "{} {}".format(sort_by, sort_order)
 
     def test_get(self):
-        assert False
+        project_id = 2
+        task_id = 3
+
+        response = self.moco.ProjectTask.get(project_id, task_id)
+
+        assert response["method"] == "GET"
 
     def test_create(self):
-        assert False
+        project_id = 2
+        name = "my new task"
+        billable = False
+        active = True
+        budget = 400
+        hourly_rate = 20
+
+        response = self.moco.ProjectTask.create(project_id, name, billable=billable, active=active, budget=budget, hourly_rate=hourly_rate)
+        data = response["data"]
+
+        assert data["name"] == name
+        assert data["billable"] == billable
+        assert data["active"] == active
+        assert data["budget"] == budget
+        assert data["hourly_rate"] == hourly_rate
+        assert response["method"] == "POST"
+
 
     def test_update(self):
-        assert False
+        project_id = 2
+        task_id = 89
+        name = "my new task"
+        billable = False
+        active = True
+        budget = 400
+        hourly_rate = 20
+
+        response = self.moco.ProjectTask.update(project_id, task_id, name=name, billable=billable, active=active, budget=budget, hourly_rate=hourly_rate)
+        data = response["data"]
+
+        assert data["name"] == name
+        assert data["billable"] == billable
+        assert data["active"] == active
+        assert data["budget"] == budget
+        assert data["hourly_rate"] == hourly_rate
+        assert response["method"] == "PUT"
 
     def test_delete(self):
-        assert False
+        project_id = 2
+        task_id = 3
+
+        response = self.moco.ProjectTask.delete(project_id, task_id)
+
+        assert response["method"] == "DELETE"
