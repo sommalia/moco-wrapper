31289588a8a19109355d22a38b4c461a93152960
sommalia <sommalia@protonmail.com>
2020-02-03 18:39:23 +0100

    release 0.2.2.post2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', "requests" ]
 
 setup_requirements = ['pytest-runner', ]
 
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sommalia/moco-wrapper',
-    version='0.2.2',
+    version='0.2.2.post2',
     zip_safe=False,
 )
