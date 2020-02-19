03bff529dec455f9dc5c72ea54148e231a40b2ea
sommalia <sommalia@protonmail.com>
2020-02-19 08:44:26 +0100

    rework project tasks tests

--- tests/integration/models/test_project_task.py
+++ tests/integration/models/test_project_task.py
@@ -4,130 +4,183 @@ from .. import IntegrationTest
 from moco_wrapper.util.response import JsonResponse, EmptyResponse, ListingResponse
 
 class TestProjectTask(IntegrationTest):
+    def get_customer(self):
+        with self.recorder.use_cassette("TestProjectTask.get_customer"):
+            customer_create = self.moco.Company.create(
+                "TestProjectTask",
+                company_type="customer"
+            )
+
+            return customer_create.data
+    
+    def get_user(self):
+        with self.recorder.use_cassette("TestProjectTask.get_user"):
+            user = self.moco.User.getlist().items[0]
+            return user
+
+    def get_project(self):
+        user = self.get_user()
+        customer = self.get_customer()
+
+        with self.recorder.use_cassette("TestProjectTask.get_project"):
+            project_create = self.moco.Project.create(
+                "dummy project, test project_task",
+                "EUR",
+                date(2020, 1, 1),
+                user.id,
+                customer.id
+            )
+
+            return project_create.data
+
     def test_get(self):
+        project = self.get_project()
+
         with self.recorder.use_cassette("TestProjectTask.test_get"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            name = "dummy task, test get"
+            billable = False
+            active = True
+            budget = 500
+            hourly_rate = 20
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "single task to be retrieved", budget=100, hourly_rate=10)
+            task_create = self.moco.ProjectTask.create(
+                project.id,
+                name,
+                billable=billable,
+                active=active,
+                budget=budget,
+                hourly_rate=hourly_rate
+            )
 
-            project_task_get = self.moco.ProjectTask.get(project_create.data.id, project_task_create.data.id)
+            task_get = self.moco.ProjectTask.get(
+                project.id, 
+                task_create.data.id
+            )
 
-            assert project_create.response.status_code == 200
-            assert project_task_create.response.status_code == 200
-            assert project_task_get.response.status_code == 200
+            assert task_create.response.status_code == 200
+            assert task_get.response.status_code == 200
 
-            assert isinstance(project_task_get, JsonResponse)
+            assert isinstance(task_create, JsonResponse)
+            assert isinstance(task_get, JsonResponse)
 
-            assert project_task_get.data.budget == 100
-            assert project_task_get.data.hourly_rate == 10
+            assert task_get.data.name == name
+            assert task_get.data.billable == billable
+            assert task_get.data.active == active
+            assert task_get.data.budget == budget
+            assert task_get.data.hourly_rate == hourly_rate
 
     def test_getlist(self):
+        project = self.get_project()
+
         with self.recorder.use_cassette("TestProjectTask.test_getlist"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            task_list = self.moco.ProjectTask.getlist(project.id)
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "task in list to be retrieved", budget=100, hourly_rate=10)
-            project_task_sec_create = self.moco.ProjectTask.create(project_create.data.id, "another task in list to be retrieved", budget=300, hourly_rate=5.5)
+            assert task_list.response.status_code == 200
 
-            project_task_getlist = self.moco.ProjectTask.getlist(project_create.data.id)
-
-            assert project_create.response.status_code == 200
-            assert project_task_create.response.status_code == 200
-            assert project_task_sec_create.response.status_code == 200
-            assert project_task_getlist.response.status_code == 200 
-
-            assert isinstance(project_task_getlist, ListingResponse)
+            assert isinstance(task_list, ListingResponse)
 
     def test_create(self):
+        project = self.get_project()
+
         with self.recorder.use_cassette("TestProjectTask.test_create"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            name = "dummy task, test create"
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "generic task")
-            project_task_adv_create = self.moco.ProjectTask.create(project_create.data.id, "more advanced task", billable=True, budget=300.5, hourly_rate=20.5)
-
-            assert project_create.response.status_code == 200
-            assert project_task_create.response.status_code == 200
-            assert project_task_adv_create.response.status_code == 200
-
-            assert isinstance(project_task_adv_create, JsonResponse)
-            assert isinstance(project_task_create, JsonResponse)
-
-            assert project_task_adv_create.data.budget == 300.5
-            assert project_task_adv_create.data.hourly_rate == 20.5
-            assert project_task_create.data.name == "generic task"
-
-    def test_create_inactive_task(self):
-        with self.recorder.use_cassette("TestProjectTask.test_create_inactive_task"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
-
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "inactive task", active=False)
-
-            assert isinstance(project_task_create, JsonResponse)
-
-            assert project_task_create.data.name == "inactive task"
-            assert project_task_create.data.active == False
-            assert project_task_create.data.billable == True
+            task_create = self.moco.ProjectTask.create(
+                project.id,
+                name
+            )
             
+            assert task_create.response.status_code == 200
 
-    def test_create_non_billable_task(self):
-        with self.recorder.use_cassette("TestProjectTask.test_create_non_billable_task"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            assert isinstance(task_create, JsonResponse)
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "non-billable task", billable=False)
+            assert task_create.data.name == name
 
-            assert isinstance(project_task_create, JsonResponse)
+    def test_create_full(self):
+        project = self.get_project()
 
-            assert project_task_create.data.name == "non-billable task"
-            assert project_task_create.data.active == True
-            assert project_task_create.data.billable == False
+        with self.recorder.use_cassette("TestProjectTask.test_create_full"):
+            name = "dummy task, test create full"
+            billable = False
+            active = True
+            budget = 500
+            hourly_rate = 20
+
+            task_create = self.moco.ProjectTask.create(
+                project.id,
+                name,
+                billable=billable,
+                active=active,
+                budget=budget,
+                hourly_rate=hourly_rate
+            )
+
+            assert task_create.response.status_code == 200
+          
+            assert isinstance(task_create, JsonResponse)
+          
+            assert task_create.data.name == name
+            assert task_create.data.billable == billable
+            assert task_create.data.active == active
+            assert task_create.data.budget == budget
+            assert task_create.data.hourly_rate == hourly_rate
 
     def test_delete(self):
+        project = self.get_project()
+
         with self.recorder.use_cassette("TestProjectTask.test_delete"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            task_create = self.moco.ProjectTask.create(
+                project.id,
+                "dummy task, test delete"
+            )
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "task to delete")
-            project_task_delete = self.moco.ProjectTask.delete(project_create.data.id, project_task_create.data.id)
+            task_delete = self.moco.ProjectTask.delete(
+                project.id,
+                task_create.data.id,
+            )
 
-            assert project_create.response.status_code == 200
-            assert project_task_create.response.status_code == 200
-            assert project_task_delete.response.status_code == 204
+            assert task_create.response.status_code == 200
+            assert task_delete.response.status_code == 204
 
-            assert isinstance(project_task_delete, EmptyResponse)
+            assert isinstance(task_delete, EmptyResponse)
+
 
     def test_update(self):
+        project = self.get_project()
+
         with self.recorder.use_cassette("TestProjectTask.test_update"):
-            user_get = self.moco.User.getlist().items[0]
-            company_get = self.moco.Company.getlist().items[0]
+            name = "dummy task, this task was successfully updated"
+            billable = False
+            active = True
+            budget = 500
+            hourly_rate = 20
 
-            project_create = self.moco.Project.create("testproject with tasks", "EUR", date(2022, 1, 1), user_get.id, company_get.id);
-            project_task_create = self.moco.ProjectTask.create(project_create.data.id, "task to be updated", billable=False, active=False, budget=100, hourly_rate=10)
-            project_task_update = self.moco.ProjectTask.update(project_create.data.id, project_task_create.data.id, billable=True, active=True, budget=200, hourly_rate=20)
+            task_create = self.moco.ProjectTask.create(
+                project.id,
+                "dummy task, test update"
+            )
 
-            project_task_get = self.moco.ProjectTask.get(project_create.data.id, project_task_create.data.id)
+            task_update = self.moco.ProjectTask.update(
+                project.id,
+                task_create.data.id,
+                name=name,
+                billable=billable,
+                active=active,
+                budget=budget,
+                hourly_rate=hourly_rate
+            )
 
-            assert project_create.response.status_code == 200
-            assert project_task_create.response.status_code == 200
-            assert project_task_update.response.status_code == 200
-            assert project_task_get.response.status_code == 200
-
-            assert isinstance(project_task_update, JsonResponse)
-            assert isinstance(project_task_get, JsonResponse)
-
-            assert project_task_get.data.billable == True
-            assert project_task_get.data.active == True
-            assert project_task_get.data.budget == 200
-            assert project_task_get.data.hourly_rate == 20
+            assert task_create.response.status_code == 200
+            assert task_update.response.status_code == 200
+          
+            assert isinstance(task_create, JsonResponse)
+            assert isinstance(task_update, JsonResponse)
+          
+            assert task_update.data.name == name
+            assert task_update.data.billable == billable
+            assert task_update.data.active == active
+            assert task_update.data.budget == budget
+            assert task_update.data.hourly_rate == hourly_rate
 
 
             
