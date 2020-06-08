f218268c7a8cc4b3a58e9eb05ef30c2a5ba14d9c
sommalia <sommalia@tuta.io>
2020-06-08 15:27:00 +0200

    include docs for bumpversion search/replace

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'sommalia'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.6.2'
+release = '0.7.2'
 
 
 # -- General configuration ---------------------------------------------------
