9f8d43c9af1c4685af26345923a7fb75c9efb385
sommalia <sommalia@tuta.io>
2020-06-25 17:15:17 +0200

    refine models base documentation

--- moco_wrapper/models/base.py
+++ moco_wrapper/models/base.py
@@ -1,20 +1,24 @@
 import datetime
 
+
 class MWRAPBase(object):
     """
-    Base class for all models in MocoWrapper.
+    Base class all other model classes inherit
     """
 
     def _convert_date_to_iso(self, date_to_convert: datetime.date):
         """
-        converts a datetime object to iso format need by the api
-        
-        :param date_to_convert: date object to convert
-        :returns: date in iso format (YYYY-MM-DD)
+        Converts a datetime object to iso format need by the api
+
+        :param date_to_convert: datetime.date object to convert
+
+        :type date_to_convert: datetime.date
+
+        :returns: Date in iso format (YYYY-MM-DD)
+        :rtype: str
         """
         return datetime.date(
             date_to_convert.year,
             date_to_convert.month,
             date_to_convert.day
         ).isoformat()
-        
