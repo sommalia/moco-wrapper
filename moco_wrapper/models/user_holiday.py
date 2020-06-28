cd7fe324152ba5a79e192de489187c38644d878a
sommalia <sommalia@tuta.io>
2020-06-28 11:08:39 +0200

    fix holyday typo (holyday -> holiday), add rtype and default value documentation to user holiday model

--- moco_wrapper/models/user_holiday.py
+++ moco_wrapper/models/user_holiday.py
@@ -1,13 +1,12 @@
 from moco_wrapper.models.base import MWRAPBase
 from moco_wrapper.const import API_PATH
 
+
 class UserHoliday(MWRAPBase):
     """
     Class for handling users holiday/vacation credits.
 
     Every user that can take vacation has a number of vacation credits. These can be manged with this model.
-    
-
 
     Example usage:
 
@@ -15,16 +14,19 @@ class UserHoliday(MWRAPBase):
 
         from moco_wrapper import Moco
 
-        user_id = 22 #this user gets extra vacation in 2020
+        m = Moco()
+
+        # this user gets extra vacation in 2020
         m.UserHoliday.create(
-            2020,
-            "extra vacation day",
-            8 #hours
+            year = 2020,
+            title = "extra vacation day",
+            user_id = 22,
+            hours = 8
         )
 
     .. note::
-
-        Please not that the base unit for holiday credits is hours. So if your typical workday contains 8 hours, 8 holiday credits means one day off.
+        Please not that the base unit for holiday credits is hours. So if your typical workday contains 8 hours,
+        8 holiday credits means one day off.
     """
 
     def __init__(self, moco):
@@ -42,15 +44,15 @@ class UserHoliday(MWRAPBase):
         sort_by: str = None,
         sort_order: str = 'asc',
         page: int = 1
-        ):
+    ):
         """
-        Retrieve a list of holidays entries
+        Retrieve a list of holiday entries.
 
-        :param year: Show only this year (e.g. 2019)
-        :param user_id: Show only holidays from this user (e.g. 5)
-        :param sort_by: field to sort results by
-        :param sort_order: asc or desc
-        :param page: Page number (default 1)
+        :param year: Show only this year (e.g. 2019) (default ``None``)
+        :param user_id: Show only holidays from this user (e.g. 5) (default ``None``)
+        :param sort_by: field to sort results by (default ``None``)
+        :param sort_order: asc or desc (default ``"asc"``)
+        :param page: Page number (default ``1``)
 
         :type year: int
         :type user_id: int
@@ -59,6 +61,7 @@ class UserHoliday(MWRAPBase):
         :type page: int
 
         :returns: List of holiday entries
+        :rtype: :class:`moco_wrapper.util.response.ListingResponse`
         """
         params = {}
         for key, value in (
@@ -70,41 +73,42 @@ class UserHoliday(MWRAPBase):
                 params[key] = value
 
         if sort_by is not None:
-            params["sort_by"] = "{} {}".format(sort_by, sort_order) 
+            params["sort_by"] = "{} {}".format(sort_by, sort_order)
 
         return self._moco.get(API_PATH["holiday_getlist"], params=params)
-    
+
     def get(
         self,
-        holyday_id: int
-        ):
+        holiday_id: int
+    ):
         """
         Retrieve single holiday entry
 
-        :param holyday_id: Id of the holiday
+        :param holiday_id: Id of the holiday
 
-        :type holyday_id: int
+        :type holiday_id: int
 
         :returns: The holiday object
+        :rtype: :class:`moco_wrapper.util.response.JsonResponse`
         """
-        return self._moco.get(API_PATH["holiday_get"].format(id=holyday_id))
+        return self._moco.get(API_PATH["holiday_get"].format(id=holiday_id))
 
     def create(
         self,
-        year: int, 
+        year: int,
         title: str,
         user_id: int,
         hours: float = None,
         days: float = None,
-        ):
+    ):
         """
-        Create an users entitlement for holidays/vacation (either by hours or days)
+        Create an users entitlement for holidays/vacation (either by hours or days).
 
         :param year: Year the holiday credits are for
-        :param title: Title 
+        :param title: Title
         :param user_id: Id of the user these holiday credits belongs to
-        :param hours: Hours (specify either hours or days)
-        :param days: Days (specify either hours or days)
+        :param hours: Hours (specify either hours or days) (default ``None``)
+        :param days: Days (specify either hours or days) (default ``None``)
 
         :type year: int
         :type title: str
@@ -113,6 +117,7 @@ class UserHoliday(MWRAPBase):
         :type days: float
 
         :returns: The created holiday object
+        :rtype: :class:`moco_wrapper.util.response.JsonResponse`
         """
 
         if days is None and hours is None:
@@ -121,9 +126,8 @@ class UserHoliday(MWRAPBase):
         if days is not None and hours is not None:
             raise ValueError("Specify either hours or the days parameter (not both)")
 
-
         data = {
-            "year" : year,
+            "year": year,
             "title": title,
             "user_id": user_id,
         }
@@ -141,40 +145,40 @@ class UserHoliday(MWRAPBase):
 
     def update(
         self,
-        holyday_id: int,
+        holiday_id: int,
         year: int = None,
         title: str = None,
         user_id: int = None,
         hours: float = None,
         days: float = None,
-        ):
+    ):
         """
         Update a holiday entry
 
-        :param holyday_id: Id of the holiday entry
-        :param year:  Year the holiday credits are for
-        :param title: Title
-        :param user_id: User this holiday entry belongs to
-        :param hours: Hours (specify either hours or days)
-        :param days: Days (specify either hours or days)
+        :param holiday_id: Id of the holiday entry
+        :param year:  Year the holiday credits are for (default ``None``)
+        :param title: Title (default ``None``)
+        :param user_id: User this holiday entry belongs to (default ``None``)
+        :param hours: Hours (specify either hours or days) (default ``None``)
+        :param days: Days (specify either hours or days) (default ``None``)
 
-        :type holyday_id: int
+        :type holiday_id: int
         :type year: int
         :type title: str
         :type user_id: int
         :type hours: float
         :type days: float
-        
+
         :returns: The updated holiday object
+        :rtype: :class:`moco_wrapper.util.response.JsonResponse`
         """
-  
+
         if days is None and hours is None:
             raise ValueError("Either the hours or days parameter must be specified")
 
         if days is not None and hours is not None:
             raise ValueError("Specify either hours or the days parameter (not both)")
 
-        
         data = {}
         for key, value in (
             ("year", year),
@@ -186,19 +190,20 @@ class UserHoliday(MWRAPBase):
             if value is not None:
                 data[key] = value
 
-        return self._moco.put(API_PATH["holiday_update"].format(id=holyday_id), data=data)
+        return self._moco.put(API_PATH["holiday_update"].format(id=holiday_id), data=data)
 
     def delete(
         self,
-        holyday_id: int
-        ):
+        holiday_id: int
+    ):
         """
         Delete a holiday entry
 
-        :param holyday_id: Id of the holiday entry to delete
+        :param holiday_id: Id of the holiday entry to delete
 
-        :type holyday_id: int
+        :type holiday_id: int
 
         :returns: Empty response on success
+        :rtype: :class:`moco_wrapper.util.response.EmptyResponse`
         """
-        return self._moco.delete(API_PATH["holiday_delete"].format(id=holyday_id))
+        return self._moco.delete(API_PATH["holiday_delete"].format(id=holiday_id))
