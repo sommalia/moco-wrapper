2c224b9f19cf087ecef215875727cce4f636b7bc
sommalia <sommalia@tuta.io>
2020-11-23 12:57:32 +0100

    implement integration tests for taggings

---
+++ tests/integration/models/test_tagging.py
@@ -0,0 +1,69 @@
+from moco_wrapper.models.company import CompanyType
+from moco_wrapper.models.tagging import TaggingEntity
+from moco_wrapper.util.response import ListResponse
+
+from .. import IntegrationTest
+
+
+class TestTagging(IntegrationTest):
+
+    def get_company(self):
+        with self.recorder.use_cassette("TestTagging.get_company"):
+            company_create = self.moco.Company.create(
+                name="TestTagging Company",
+                company_type=CompanyType.CUSTOMER
+            )
+
+            return company_create.data
+
+    def test_add(self):
+        company = self.get_company()
+        tags = ["test tagging", "add", "tags"]
+
+        with self.recorder.use_cassette("TestTagging.test_add"):
+            tagging_add = self.moco.Tagging.add(
+                entity=TaggingEntity.COMPANY,
+                entity_id=company.id,
+                tags=tags
+            )
+
+            assert tagging_add.response.status_code == 200
+
+            assert isinstance(tagging_add, ListResponse)
+
+            for tag in tags:
+                assert tag in tagging_add.items
+
+    def test_replace(self):
+        company = self.get_company()
+        tags = ["test tagging replace", "replace", "tags"]
+
+        with self.recorder.use_cassette("TestTagging.test_replace"):
+            tagging_replace = self.moco.Tagging.replace(
+                entity=TaggingEntity.COMPANY,
+                entity_id=company.id,
+                tags=tags
+            )
+            assert tagging_replace.response.status_code == 200
+
+            assert isinstance(tagging_replace, ListResponse)
+
+            assert sorted(tagging_replace.data) == sorted(tags)
+
+    def test_delete(self):
+        company = self.get_company()
+        tags = ["test tagging delete", "delete", "tags"]
+
+        with self.recorder.use_cassette("TestTagging.test_delete"):
+            tagging_delete = self.moco.Tagging.delete(
+                entity=TaggingEntity.COMPANY,
+                entity_id=company.id,
+                tags=tags
+            )
+
+            assert tagging_delete.response.status_code == 200
+
+            assert isinstance(tagging_delete, ListResponse)
+
+            for tag in tags:
+                assert tag not in tagging_delete.items
